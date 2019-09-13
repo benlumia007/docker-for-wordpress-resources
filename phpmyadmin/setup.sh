e7520ce3690fe167bf20cd629e2b7c3e5169ff9f
Benjamin Lu <benlumia007@gmail.com>
2019-09-13 10:28:15 -0700

    this has been updated so it downloads and installs phpmyadmin

---
+++ phpmyadmin/setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [[ ! -d "/sites/dashboard/public_html/phpmyadmin" ]]; then
+    mkdir -p "/sites/dashboard/public_html/phpmyadmin"
+    cd "/sites/dashboard/public_html/phpmyadmin"
+    echo "extracting phpMyAdmin-4.8.5-all-languages"
+    wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.zip -O phpmyadmin.zip
+    unzip phpmyadmin.zip -d "/sites/dashboard/public_html/phpmyadmin"
+    mv phpMyAdmin-4.9.0.1-all-languages/* "/sites/dashboard/public_html/phpmyadmin"
+    rm -rf phpMyAdmin-4.9.0.1-all-languages
+    rm phpmyadmin.zip
+fi
