b2e35343a7da66e7f534087e76dd3f0eebf5c4db
Benjamin Lu <benlumia007@gmail.com>
2019-09-13 11:19:46 -0700

    fixed

--- phpmyadmin/setup.sh
+++ phpmyadmin/setup.sh
@@ -2,11 +2,10 @@
 
 if [[ ! -d "sites/dashboard/public_html/phpmyadmin" ]]; then
     mkdir -p "sites/dashboard/public_html/phpmyadmin"
-    cd "sites/dashboard/public_html/phpmyadmin"
     echo "extracting phpMyAdmin-4.8.5-all-languages"
-    wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.zip -O phpmyadmin.zip
-    unzip phpmyadmin.zip -d "sites/dashboard/public_html/phpmyadmin"
+    wget https://files.phpmyadmin.net/phpMyAdmin/4.9.0.1/phpMyAdmin-4.9.0.1-all-languages.zip -O "sites/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
+    unzip "sites/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
     mv phpMyAdmin-4.9.0.1-all-languages/* "sites/dashboard/public_html/phpmyadmin"
     rm -rf phpMyAdmin-4.9.0.1-all-languages
-    rm phpmyadmin.zip
+    rm "sites/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
 fi
