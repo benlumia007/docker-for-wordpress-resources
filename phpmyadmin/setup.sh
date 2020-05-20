a16b6daffe0f4b61a6913b2a21a3cac65d04c393
benlumia007 <benlumia007@users.noreply.github.com>
2020-05-19 22:37:57 -0700

    Update setup.sh

--- phpmyadmin/setup.sh
+++ phpmyadmin/setup.sh
@@ -3,10 +3,10 @@
 if [[ ! -d "sites/dashboard/public_html/phpmyadmin" ]]; then
     mkdir -p "sites/dashboard/public_html/phpmyadmin"
     echo "extracting phpMyAdmin-5.0.1-all-languages"
-    wget https://files.phpmyadmin.net/phpMyAdmin/5.0.1/phpMyAdmin-5.0.1-all-languages.zip -O "sites/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
+    wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip -O "sites/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
     unzip "sites/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
-    mv phpMyAdmin-5.0.1-all-languages/* "sites/dashboard/public_html/phpmyadmin"
-    rm -rf phpMyAdmin-5.0.1-all-languages
+    mv phpMyAdmin-5.0.2-all-languages/* "sites/dashboard/public_html/phpmyadmin"
+    rm -rf phpMyAdmin-5.0.2-all-languages
     rm "sites/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
     cp "config/phpmyadmin/config.inc.php" "sites/dashboard/public_html/phpmyadmin"
 fi
