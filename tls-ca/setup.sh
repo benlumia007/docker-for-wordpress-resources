d7078220459c7fd27024287b54a39d6f6b56ef07
benlumia007 <benlumia007@users.noreply.github.com>
2020-05-24 17:55:01 -0700

    Update setup.sh

--- tls-ca/setup.sh
+++ tls-ca/setup.sh
@@ -28,14 +28,14 @@ for domain in `get_sites`; do
     provision=`cat ${config} | shyaml get-value sites.${domain}.provision`
 
     if [[ "True" == ${provision} ]]; then
-        if [[ ! -f "certificates/${domain}.crt" ]]; then
-            cp "config/certs/domain.ext" "certificates/${domain}.ext"
-            sed -i -e "s/{{DOMAIN}}/${domain}/g" "certificates/${domain}.ext"
-            rm -rf "certificates/${domain}.ext-e"
+        if [[ ! -f "certificates/${domain}/${domain}.crt" ]]; then
+            cp "config/certs/domain.ext" "certificates/${domain}/${domain}.ext"
+            sed -i -e "s/{{DOMAIN}}/${domain}/g" "certificates/${domain}/${domain}.ext"
+            rm -rf "certificates/${domain}/${domain}.ext-e"
 
-            openssl genrsa -out "certificates/${domain}.key" 4096 &> /dev/null
-            openssl req -new -key "certificates/${domain}.key" -out "certificates/${domain}.csr" -subj "/CN=*.${domain}.test" &> /dev/null
-            openssl x509 -req -in "certificates/${domain}.csr" -CA "certificates/ca.crt" -CAkey "certificates/ca.key" -CAcreateserial -out "certificates/${domain}.crt" -days 365 -sha256 -extfile "certificates/${domain}.ext" &> /dev/null
+            openssl genrsa -out "certificates/${domain}/${domain}.key" 4096 &> /dev/null
+            openssl req -new -key "certificates/${domain}/${domain}.key" -out "certificates/${domain}/${domain}.csr" -subj "/CN=*.${domain}.test" &> /dev/null
+            openssl x509 -req -in "certificates/${domain}/${domain}.csr" -CA "certificates/ca/ca.crt" -CAkey "certificates/ca/ca.key" -CAcreateserial -out "certificates/${domain}/${domain}.crt" -days 365 -sha256 -extfile "certificates/${domain}/${domain}.ext" &> /dev/null
         fi
     fi
 done
