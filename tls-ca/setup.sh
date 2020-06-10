78c3d89cbdb966e737e39cca7ee1038eccc12887
benlumia007 <benlumia007@users.noreply.github.com>
2020-06-10 16:59:52 -0700

    Update setup.sh

--- tls-ca/setup.sh
+++ tls-ca/setup.sh
@@ -31,15 +31,15 @@ for domain in ${domains//- /$'\n'}; do
     provision=`cat ${config} | shyaml get-value sites.${domain}.provision`
 
     if [[ "True" == ${provision} ]]; then
-        if [[ ! -f "certificates/${domain}/${domain}.crt" ]]; then
-            mkdir -p "certificates/${domain}"
-            cp "config/certs/domain.ext" "certificates/${domain}/${domain}.ext"
-            sed -i -e "s/{{DOMAIN}}/${domain}/g" "certificates/${domain}/${domain}.ext"
-            rm -rf "certificates/${domain}/${domain}.ext-e"
+        if [[ ! -f "/srv/certificates/${domain}/${domain}.crt" ]]; then
+            mkdir -p "/srv/certificates/${domain}"
+            cp "config/certs/domain.ext" "/srv/certificates/${domain}/${domain}.ext"
+            sed -i -e "s/{{DOMAIN}}/${domain}/g" "/srv/certificates/${domain}/${domain}.ext"
+            rm -rf "/srv/certificates/${domain}/${domain}.ext-e"
 
-            openssl genrsa -out "certificates/${domain}/${domain}.key" 4096 &> /dev/null
-            openssl req -new -key "certificates/${domain}/${domain}.key" -out "certificates/${domain}/${domain}.csr" -subj "/CN=*.${domain}.test" &> /dev/null
-            openssl x509 -req -in "certificates/${domain}/${domain}.csr" -CA "certificates/ca/ca.crt" -CAkey "certificates/ca/ca.key" -CAcreateserial -out "certificates/${domain}/${domain}.crt" -days 365 -sha256 -extfile "certificates/${domain}/${domain}.ext" &> /dev/null
+            openssl genrsa -out "/srv/certificates/${domain}/${domain}.key" 4096 &> /dev/null
+            openssl req -new -key "/srv/certificates/${domain}/${domain}.key" -out "/srv/certificates/${domain}/${domain}.csr" -subj "/CN=*.${domain}.test" &> /dev/null
+            openssl x509 -req -in "/srv/certificates/${domain}/${domain}.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/${domain}/${domain}.crt" -days 365 -sha256 -extfile "/srv/certificates/${domain}/${domain}.ext" &> /dev/null
         fi
     fi
 done
