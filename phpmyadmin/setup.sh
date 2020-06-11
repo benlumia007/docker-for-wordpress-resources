fda134635db9db2b1ef98205322c48005bf4fb44
benlumia007 <benlumia007@users.noreply.github.com>
2020-06-10 17:10:54 -0700

    Update setup.sh

--- phpmyadmin/setup.sh
+++ phpmyadmin/setup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ ! -d "sites/dashboard/public_html/phpmyadmin" ]]; then
-    mkdir -p "sites/dashboard/public_html/phpmyadmin"
+if [[ ! -d "/srv/www/dashboard/public_html/phpmyadmin" ]]; then
+    mkdir -p "/srv/www/dashboard/public_html/phpmyadmin"
     echo "extracting phpMyAdmin-5.0.1-all-languages"
-    wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip -O "sites/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
-    unzip "sites/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
-    mv phpMyAdmin-5.0.2-all-languages/* "sites/dashboard/public_html/phpmyadmin"
+    wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip -O "/srv/www/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
+    unzip "/srv/www/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
+    mv phpMyAdmin-5.0.2-all-languages/* "/srv/www/dashboard/public_html/phpmyadmin"
     rm -rf phpMyAdmin-5.0.2-all-languages
-    rm "sites/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
-    cp "config/phpmyadmin/config.inc.php" "sites/dashboard/public_html/phpmyadmin"
+    rm "/srv/www/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
+    cp "config/phpmyadmin/config.inc.php" "/srv/www/dashboard/public_html/phpmyadmin"
 fi
