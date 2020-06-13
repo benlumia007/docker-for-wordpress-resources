7c96d681767d1422e3921e12faf075905aaea0e4
Benjamin Lu <benlumia007@users.noreply.github.com>
2020-06-13 00:44:12 -0700

    removed

--- mailcatcher/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ ! -f "/usr/local/bin/mailcatcher" ]]; then
-    sudo apt update -y
-    sudo apt install libsqlite3-dev ruby-dev -y
-    sudo gem install mailcatcher
-fi
