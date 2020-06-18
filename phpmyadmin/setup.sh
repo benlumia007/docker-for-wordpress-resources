4926af564f9d2faad9e6588810db45fe28ff81e7
benlumia007 <benlumia007@users.noreply.github.com>
2020-06-18 11:45:24 -0700

    Update setup.sh

--- phpmyadmin/setup.sh
+++ phpmyadmin/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ ! -d "/srv/www/dashboard/public_html/phpmyadmin" ]]; then
-    mkdir -p "/srv/www/dashboard/public_html/phpmyadmin"
+    noroot mkdir -p "/srv/www/dashboard/public_html/phpmyadmin"
     echo "extracting phpMyAdmin-5.0.1-all-languages"
     wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip -O "/srv/www/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
     unzip "/srv/www/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
