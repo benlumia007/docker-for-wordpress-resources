9fa11395c15a5572ecc04e8cab0e51cf4aea2abb
benlumia007 <benlumia007@users.noreply.github.com>
2020-06-18 12:12:05 -0700

    Update setup.sh

--- phpmyadmin/setup.sh
+++ phpmyadmin/setup.sh
@@ -5,4 +5,9 @@ if [[ ! -d "/srv/www/dashboard/public_html/phpmyadmin" ]]; then
     cd "/srv/www/dashboard/public_html/phpmyadmin"
     noroot wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip -O "phpmyadmin.zip"
     noroot unzip "phpmyadmin.zip"
+    noroot mv phpMyAdmin-5.0.2-all-languages/* "/srv/www/dashboard/public_html/phpmyadmin"
+    noroot rm -rf phpMyAdmin-5.0.2-all-languages
+    noroot rm "phpmyadmin.zip"
+    noroot cp "/app/config/templates/config.inc.php" "/srv/www/dashboard/public_html/phpmyadmin"
+    cd /app
 fi
