395914d8422697233c66db4b85a5a282dad10a41
benlumia007 <benlumia007@users.noreply.github.com>
2020-06-18 13:29:41 -0700

    Update setup.sh

--- tls-ca/setup.sh
+++ tls-ca/setup.sh
@@ -8,19 +8,19 @@ get_sites() {
 }
 
 if [[ ! -d "/srv/certificates/ca" ]]; then
-    mkdir -p "/srv/certificates/ca"
-    openssl genrsa -out "/srv/certificates/ca/ca.key" 4096 &> /dev/null
-    openssl req -x509 -new -nodes -key "/srv/certificates/ca/ca.key" -sha256 -days 365 -out "/srv/certificates/ca/ca.crt" -subj "/CN=Docker for WordPress" &> /dev/null
+    noroot mkdir -p "/srv/certificates/ca"
+    noroot openssl genrsa -out "/srv/certificates/ca/ca.key" 4096 &> /dev/null
+    noroot openssl req -x509 -new -nodes -key "/srv/certificates/ca/ca.key" -sha256 -days 365 -out "/srv/certificates/ca/ca.crt" -subj "/CN=Docker for WordPress" &> /dev/null
 fi
 
 if [[ ! -f "/srv/certificates/dashboard/dashboard.crt" ]]; then
-    mkdir -p "/srv/certificates/dashboard"
-    cp "/app/config/templates/certs.ext" "/srv/certificates/dashboard/dashboard.ext"
-    sed -i -e "s/{{DOMAIN}}/dashboard/g" "/srv/certificates/dashboard/dashboard.ext"
+    noroot mkdir -p "/srv/certificates/dashboard"
+    noroot cp "/app/config/templates/certs.ext" "/srv/certificates/dashboard/dashboard.ext"
+    noroot sed -i -e "s/{{DOMAIN}}/dashboard/g" "/srv/certificates/dashboard/dashboard.ext"
 
-    openssl genrsa -out "/srv/certificates/dashboard/dashboard.key" 4096 &> /dev/null
-    openssl req -new -key "/srv/certificates/dashboard/dashboard.key" -out "/srv/certificates/dashboard/dashboard.csr" -subj "/CN=*.dashboard.test" &> /dev/null
-    openssl x509 -req -in "/srv/certificates/dashboard/dashboard.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/dashboard/dashboard.crt" -days 365 -sha256 -extfile "/srv/certificates/dashboard/dashboard.ext" &> /dev/null
+    noroot openssl genrsa -out "/srv/certificates/dashboard/dashboard.key" 4096 &> /dev/null
+    noroot openssl req -new -key "/srv/certificates/dashboard/dashboard.key" -out "/srv/certificates/dashboard/dashboard.csr" -subj "/CN=*.dashboard.test" &> /dev/null
+    noroot openssl x509 -req -in "/srv/certificates/dashboard/dashboard.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/dashboard/dashboard.crt" -days 365 -sha256 -extfile "/srv/certificates/dashboard/dashboard.ext" &> /dev/null
 fi
 
 domains=`get_sites`
@@ -30,14 +30,14 @@ for domain in ${domains//- /$'\n'}; do
 
     if [[ "True" == ${provision} ]]; then
         if [[ ! -f "/srv/certificates/${domain}/${domain}.crt" ]]; then
-            mkdir -p "/srv/certificates/${domain}"
-            cp "/app/config/templates/certs.ext" "/srv/certificates/${domain}/${domain}.ext"
-            sed -i -e "s/{{DOMAIN}}/${domain}/g" "/srv/certificates/${domain}/${domain}.ext"
-            rm -rf "/srv/certificates/${domain}/${domain}.ext-e"
+            noroot mkdir -p "/srv/certificates/${domain}"
+            noroot cp "/app/config/templates/certs.ext" "/srv/certificates/${domain}/${domain}.ext"
+            noroot sed -i -e "s/{{DOMAIN}}/${domain}/g" "/srv/certificates/${domain}/${domain}.ext"
+            noroot rm -rf "/srv/certificates/${domain}/${domain}.ext-e"
 
-            openssl genrsa -out "/srv/certificates/${domain}/${domain}.key" 4096 &> /dev/null
-            openssl req -new -key "/srv/certificates/${domain}/${domain}.key" -out "/srv/certificates/${domain}/${domain}.csr" -subj "/CN=*.${domain}.test" &> /dev/null
-            openssl x509 -req -in "/srv/certificates/${domain}/${domain}.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/${domain}/${domain}.crt" -days 365 -sha256 -extfile "/srv/certificates/${domain}/${domain}.ext" &> /dev/null
+            noroot openssl genrsa -out "/srv/certificates/${domain}/${domain}.key" 4096 &> /dev/null
+            noroot openssl req -new -key "/srv/certificates/${domain}/${domain}.key" -out "/srv/certificates/${domain}/${domain}.csr" -subj "/CN=*.${domain}.test" &> /dev/null
+            noroot openssl x509 -req -in "/srv/certificates/${domain}/${domain}.csr" -CA "/srv/certificates/ca/ca.crt" -CAkey "/srv/certificates/ca/ca.key" -CAcreateserial -out "/srv/certificates/${domain}/${domain}.crt" -days 365 -sha256 -extfile "/srv/certificates/${domain}/${domain}.ext" &> /dev/null
         fi
     fi
 done
