e5b18f7c3a94d140e0420ceaf3a3ae99fa517faf
benlumia007 <benlumia007@users.noreply.github.com>
2020-06-18 11:46:50 -0700

    Update setup.sh

--- phpmyadmin/setup.sh
+++ phpmyadmin/setup.sh
@@ -2,11 +2,10 @@
 
 if [[ ! -d "/srv/www/dashboard/public_html/phpmyadmin" ]]; then
     noroot mkdir -p "/srv/www/dashboard/public_html/phpmyadmin"
-    echo "extracting phpMyAdmin-5.0.1-all-languages"
-    wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip -O "/srv/www/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
-    unzip "/srv/www/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
-    mv phpMyAdmin-5.0.2-all-languages/* "/srv/www/dashboard/public_html/phpmyadmin"
-    rm -rf phpMyAdmin-5.0.2-all-languages
-    rm "/srv/www/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
-    cp "/app/config/templates/config.inc.php" "/srv/www/dashboard/public_html/phpmyadmin"
+    noroot wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.zip -O "/srv/www/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
+    noroot unzip "/srv/www/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
+    noroot mv phpMyAdmin-5.0.2-all-languages/* "/srv/www/dashboard/public_html/phpmyadmin"
+    noroot rm -rf phpMyAdmin-5.0.2-all-languages
+    noroot rm "/srv/www/dashboard/public_html/phpmyadmin/phpmyadmin.zip"
+    noroot cp "/app/config/templates/config.inc.php" "/srv/www/dashboard/public_html/phpmyadmin"
 fi
