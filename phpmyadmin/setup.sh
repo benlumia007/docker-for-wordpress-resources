acd5d7003b765752a214a5623f071d4f5fa94127
benlumia007 <benlumia007@users.noreply.github.com>
2021-04-02 22:34:12 -0700

    update to 5.1.0
    update to 5.1.0

--- phpmyadmin/setup.sh
+++ phpmyadmin/setup.sh
@@ -3,12 +3,11 @@
 if [[ ! -d "/srv/www/dashboard/public_html/phpmyadmin" ]]; then
     noroot mkdir -p "/srv/www/dashboard/public_html/phpmyadmin"
     cd "/srv/www/dashboard/public_html/phpmyadmin"
-    noroot wget -q https://files.phpmyadmin.net/phpMyAdmin/5.0.4/phpMyAdmin-5.0.4-all-languages.zip -O "phpmyadmin.zip"
+    noroot wget -q https://files.phpmyadmin.net/phpMyAdmin/5.1.0/phpMyAdmin-5.1.0-all-languages.zip -O "phpmyadmin.zip"
     noroot unzip -q "phpmyadmin.zip"
-    noroot mv phpMyAdmin-5.0.4-all-languages/* "/srv/www/dashboard/public_html/phpmyadmin"
-    noroot rm -rf phpMyAdmin-5.0.4-all-languages
+    noroot mv phpMyAdmin-5.1.0-all-languages/* "/srv/www/dashboard/public_html/phpmyadmin"
+    noroot rm -rf phpMyAdmin-5.1.0-all-languages
     noroot rm "phpmyadmin.zip"
     noroot cp "/app/config/templates/config.inc.php" "/srv/www/dashboard/public_html/phpmyadmin"
     cd /app
 fi
-
